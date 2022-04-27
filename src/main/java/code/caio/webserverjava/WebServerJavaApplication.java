52d76e019b4b78e379ff19d44e7c889f81e7568d
caioreis123 <reis.caio@gmail.com>
2022-04-27 18:25:06 -0300

    add maven configuration

---
+++ src/main/java/code/caio/webserverjava/WebServerJavaApplication.java
@@ -0,0 +1,13 @@
+package code.caio.webserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/caio/webserverjava/WebServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package code.caio.webserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
